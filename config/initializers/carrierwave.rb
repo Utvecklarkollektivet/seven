ce0613040991e207942066310673c69077dcd6f6
Christoffer Artmann <christoffer.artmann@glesys.se>
2015-12-10 21:59:17 +0100

    Fog config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,10 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',
+      :region             		=> 'eu-west-1',
       :aws_access_key_id      => ENV["AWS_KEY"],
       :aws_secret_access_key  => ENV["AWS_SECRET"]
     }
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 	end
 end
