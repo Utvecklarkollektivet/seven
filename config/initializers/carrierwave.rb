a02e41bd52f9b38e97b7f5d594497e8b19f9ea20
Christoffer Artmann <christoffer.artmann@glesys.se>
2015-12-10 21:52:14 +0100

    Fog Gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.storage = :file
 
 	if Rails.env.production?
-		config.fog_directory  = "uk-prod"
+		config.fog_directory = "uk-prod"
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',
