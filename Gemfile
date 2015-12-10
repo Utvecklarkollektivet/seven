d7dedbc98e3e6abfadd253571d1d546abd800c43
Christoffer Artmann <christoffer.artmann@glesys.se>
2015-12-10 21:34:32 +0100

    Use s3 for prod images

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 gem 'carrierwave'
+gem "fog-aws"
 gem 'redcarpet'
 gem 'friendly_id'
 gem 'materialize-sass'
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  config.storage = :file
+
+	if Rails.env.production?
+		config.fog_directory  = "uk-prod"
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV["AWS_KEY"],
+      :aws_secret_access_key  => ENV["AWS_SECRET"]
+    }
+	end
+end
